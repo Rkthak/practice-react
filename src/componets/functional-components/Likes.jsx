e4968082caaa1b1bf9dd73e6bb88838b926bccd3
Rkthak <rkon2609@gmail.com>
2026-04-26 11:42:00 +0530

    form handling using mutiple states

--- src/componets/functional-components/Likes.jsx
+++ src/componets/functional-components/Likes.jsx
@@ -1,56 +1,39 @@
-// state: a way to store and manage data in a component (component's memory)
-// state is used to store data that can change over time and affect the rendering of the component
-// in react, we can use useState hook to manage state in functional components
-// the useState is a function that returns an array
-// the array have a stateful value and a function to update it.
-
-// Note: We should not update or mutate the state directly.
-// Note: State change causes re-rendering.
-import { useState } from "react";
+import React, { useState } from "react";
 
 const Likes = () => {
-  let [reactions, setReactions] = useState({
-    likes: 0,
-    dislikes: 0,
-  });
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  const [history, setHisttory] = useState([]);
-
-  // when the like button is clicked, it will call likeHandler function
-  const likeHandler = () => {
-    setReactions({
-      ...reactions,
-      likes: reactions.likes + 1,
-    });
-    setHisttory([...history, "L"]);
+  const handleLogin = (e) => {
+    e.preventDefault();
+    console.log("Logged in ..", "\t", email, "\t", password);
   };
-
-  const handleDislike = () => {
-    setReactions({
-      ...reactions,
-      dislikes: reactions.dislikes + 1,
-    });
-    setHisttory([...history, "D"]);
-  };
-
   return (
-    <>
-      {/* <h1>Likes: {reactions.like}</h1> */}
-      <button onClick={likeHandler}>
-        <span className="material-symbols-outlined">thumb_up</span>{" "}
-        {reactions.likes}
-      </button>
-      &nbsp;
-      <button onClick={handleDislike}>
-        <span className="material-symbols-outlined">thumb_down</span>{" "}
-        {reactions.dislikes}
-      </button>
-      <ul>
-        {history.map((item, index) => {
-          return <li key={index}>{item}</li>;
-        })}
-      </ul>
-    </>
+    <form onSubmit={handleLogin}>
+      <h2>Login</h2>
+      <div>
+        <input
+          type="email"
+          name={email}
+          id="email"
+          placeholder="email ..."
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+      <div>
+        <input
+          type="password"
+          name={password}
+          id="password"
+          placeholder="password ..."
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+
+      <button type="submit">submit</button>
+    </form>
   );
 };
 
