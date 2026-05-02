2515824743dbe86de08bbdf53348f1b9691a83e5
Rkthak <rkon2609@gmail.com>
2026-05-02 12:37:54 +0530

    fetching data with promises

--- src/componets/functional-components/Likes.jsx
+++ src/componets/functional-components/Likes.jsx
@@ -1,39 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Likes = () => {
-  const recipes = [
-    {
-      name: "Classic Margherita Pizza",
-      prepTime: "30 minutes",
-      difficulty: "Medium",
-      mainIngredient: "Mozzarella",
-    },
-    {
-      name: "Avocado Toast",
-      prepTime: "5 minutes",
-      difficulty: "Easy",
-      mainIngredient: "Avocado",
-    },
-    {
-      name: "Spaghetti Carbonara",
-      prepTime: "20 minutes",
-      difficulty: "Medium",
-      mainIngredient: "Eggs",
-    },
-    {
-      name: "Grilled Salmon",
-      prepTime: "15 minutes",
-      difficulty: "Easy",
-      mainIngredient: "Salmon",
-    },
-  ];
+  const [recipes, setRecipe] = useState([]);
+
+  useEffect(() => {
+    fetch("https://69f598aafb098eb7f0b55dc2.mockapi.io/recipe")
+      .then((reponse) => reponse.json())
+      .then((data) => setRecipe(data))
+      .catch((error) => console.log(error));
+  }, []);
+
   return (
     <div className="container p-2">
       <h1 className="text-2xl font-medium text-purple-500">Recipies</h1>
-      <ul className="flex justify-between mt-3.5">
+      <ul className="flex justify-between flex-wrap mt-3.5 gap-9">
         {recipes.map((recipe, index) => {
           return (
-            <li key={index} className="border-2 border-gray-300 p-2">
+            <li key={index} className="border-2 border-gray-300 p-2 min-w-2xs">
               <h1 className="mb-1 font-semibold"> {recipe.name} </h1>
               <hr />
               <p>Ingredient: {recipe.mainIngredient} </p>
