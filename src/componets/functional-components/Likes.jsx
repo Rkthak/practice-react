fc6d4822e0222ebc94974cbaa37737195e9ad4e5
Rkthak <rkon2609@gmail.com>
2026-05-02 11:50:47 +0530

    data with array of objects

--- src/componets/functional-components/Likes.jsx
+++ src/componets/functional-components/Likes.jsx
@@ -2,18 +2,44 @@ import React from "react";
 
 const Likes = () => {
   const recipes = [
-    "biryani",
-    "samosa",
-    "burger",
-    "pizza",
-    "onion garlic bread sanwich",
+    {
+      name: "Classic Margherita Pizza",
+      prepTime: "30 minutes",
+      difficulty: "Medium",
+      mainIngredient: "Mozzarella",
+    },
+    {
+      name: "Avocado Toast",
+      prepTime: "5 minutes",
+      difficulty: "Easy",
+      mainIngredient: "Avocado",
+    },
+    {
+      name: "Spaghetti Carbonara",
+      prepTime: "20 minutes",
+      difficulty: "Medium",
+      mainIngredient: "Eggs",
+    },
+    {
+      name: "Grilled Salmon",
+      prepTime: "15 minutes",
+      difficulty: "Easy",
+      mainIngredient: "Salmon",
+    },
   ];
   return (
-    <div>
-      <h1>Recipies</h1>
-      <ul>
+    <div className="container p-2">
+      <h1 className="text-2xl font-medium text-purple-500">Recipies</h1>
+      <ul className="flex justify-between mt-3.5">
         {recipes.map((recipe, index) => {
-          return <li key={index}> {recipe.toUpperCase()} </li>;
+          return (
+            <li key={index} className="border-2 border-gray-300 p-2">
+              <h1 className="mb-1 font-semibold"> {recipe.name} </h1>
+              <hr />
+              <p>Ingredient: {recipe.mainIngredient} </p>
+              <p> {recipe.prepTime} </p> <p> {recipe.difficulty} </p>
+            </li>
+          );
         })}
       </ul>
     </div>
