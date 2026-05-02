66a9ed3bc56fc1546e423cd46e2e8adc2e8c7854
Rkthak <rkon2609@gmail.com>
2026-05-02 16:17:25 +0530

    conditional rendering

--- src/componets/functional-components/Likes.jsx
+++ src/componets/functional-components/Likes.jsx
@@ -1,24 +1,55 @@
-import axios from "axios";
-import React, { useEffect, useState } from "react";
+import { FaHeart } from "react-icons/fa";
+import { FaRegHeart } from "react-icons/fa";
 
 const Likes = () => {
-  const [recipes, setRecipe] = useState([]);
-
-  useEffect(() => {
-    axios
-      .get("https://69f598aafb098eb7f0b55dc2.mockapi.io/recipe")
-      .then((data) => setRecipe(data.data))
-      .catch((error) => console.log(error));
-  }, []);
+  const recipes = [
+    {
+      name: "Classic Margherita Pizza",
+      prepTime: "30 minutes",
+      difficulty: "Medium",
+      mainIngredient: "Mozzarella",
+      isFavourite: false,
+    },
+    {
+      name: "Avocado Toast",
+      prepTime: "5 minutes",
+      difficulty: "Easy",
+      mainIngredient: "Avocado",
+      isFavourite: true,
+    },
+    {
+      name: "Spaghetti Carbonara",
+      prepTime: "20 minutes",
+      difficulty: "Medium",
+      mainIngredient: "Eggs",
+      isFavourite: false,
+    },
+    {
+      name: "Grilled Salmon",
+      prepTime: "15 minutes",
+      difficulty: "Easy",
+      mainIngredient: "Salmon",
+      isFavourite: true,
+    },
+  ];
 
   return (
     <div className="container p-2">
       <h1 className="text-2xl font-medium text-purple-500">Recipies</h1>
-      <ul className="flex justify-between flex-wrap mt-3.5 gap-9">
+      <ul className="flex flex-wrap justify-between mt-3.5 gap-5">
         {recipes.map((recipe, index) => {
           return (
             <li key={index} className="border-2 border-gray-300 p-2 min-w-2xs">
-              <h1 className="mb-1 font-semibold"> {recipe.name} </h1>
+              <div className="flex justify-between items-center">
+                <h1 className="mb-1 font-semibold"> {recipe.name} </h1>
+                <span>
+                  {recipe.isFavourite ? (
+                    <FaHeart color="red" />
+                  ) : (
+                    <FaRegHeart />
+                  )}
+                </span>
+              </div>
               <hr />
               <p>Ingredient: {recipe.mainIngredient} </p>
               <p> {recipe.prepTime} </p> <p> {recipe.difficulty} </p>
