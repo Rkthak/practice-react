e66313f29b22f000878a112fc7509cad220dd993
Rkthak <rkon2609@gmail.com>
2026-05-28 14:04:45 +0530

    clean up

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,9 @@
-import { useDispatch, useSelector } from "react-redux";
-
-import {
-  selectDislikes,
-  selectLikes,
-  selectReactions,
-  setDislikes,
-  setLikes,
-  setReactions,
-} from "./Redux/Features/reactionSlice";
+import React from 'react'
 
 const App = () => {
-  const likes = useSelector(selectLikes);
-  const dispatch = useDispatch();
-
-  const dislikes = useSelector(selectDislikes);
-
-  const reactions = useSelector(selectReactions);
-
-  const handleLikes = () => {
-    dispatch(setLikes());
-    dispatch(setReactions("LIKE"));
-  };
-
-  const handleDislikes = () => {
-    dispatch(setDislikes());
-    dispatch(setReactions("DISLIKE"));
-  };
-
   return (
-    <div>
-      <h1>Likes: {likes}</h1>
-      <button onClick={handleLikes}>Like</button> &nbsp;
-      <h1>Dislikes: {dislikes}</h1>
-      <button onClick={handleDislikes}>Dislike</button>
-      <h1>Reactions: </h1> {reactions.join(", ")}
-    </div>
-  );
-};
+    <div>App</div>
+  )
+}
 
-export default App;
+export default App
